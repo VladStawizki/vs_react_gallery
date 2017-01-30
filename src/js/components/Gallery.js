d04802ec0a40aa1c559d114b59a3d918770e34e0
Vlad Stawizki <vlad.stawizki@gmail.com>
2017-01-30 21:38:13 +0100

    default rendering for thumbnails

--- src/js/components/Gallery.js
+++ src/js/components/Gallery.js
@@ -1,7 +1,40 @@
 import React from "react";
+
 import Arrow from "./Arrow";
+import Image from "./Image";
 
 export default class Gallery extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            "thumbnails": [
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+                <Image thumb="images/dummy.jpg" />,
+            ],
+            "images": [
+                "http://lorempixel.com/1920/1080",
+                "http://lorempixel.com/1920/1080",
+                "http://lorempixel.com/1080/1920",
+                "http://lorempixel.com/1920/1080",
+                "http://lorempixel.com/1920/1080",
+                "http://lorempixel.com/1920/1080",
+                "http://lorempixel.com/1080/1920",
+                "http://lorempixel.com/1080/1920",
+                "http://lorempixel.com/1920/1080",
+                "http://lorempixel.com/1920/1080"
+            ]
+        }
+    }
+
     render() {
         return (
             <div class="pg-gallery">
@@ -12,7 +45,7 @@ export default class Gallery extends React.Component {
                     <Arrow direction="right" />
                 </div>
                 <div class="pg-canvas">
-
+                    {this.state.thumbnails}
                 </div>
             </div>
         );
