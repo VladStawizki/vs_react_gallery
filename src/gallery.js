9af521b88a87a3e9d49d4d048403cb8330000dc3
Vlad Stawizki <vlad.stawizki@gmail.com>
2017-03-11 16:58:52 +0100

    upgrade to webpack2 and add scss styles

--- src/gallery.js
+++ src/gallery.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Gallery from "./components/Gallery";
+import Gallery from "./js/components/Gallery";
+
+import styles from "./scss/index.scss";
+
+
 
 const gallery = document.getElementById('gallery');
 ReactDOM.render(<Gallery/>, gallery);
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,48 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/gallery.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
-      }
+    context: path.resolve(__dirname, './src'),
+
+    entry: {
+        gallery: './gallery.js'
+    },
+
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modues/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'es2015', 'stage-0'],
+                        plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+                    }
+                }]
+            },
+            {
+                test: /\.(sass|scss)$/,
+                exclude: [/node_modues/],
+                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            }
+        ]
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist/assets'),
+        filename: '[name].bundle.js',
+        publicPath: '/assets'
+    },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, './src')
+    },
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].bundle.css'
+        })
     ]
-  },
-  output: {
-    path: __dirname + "/src/",
-    filename: "gallery.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
-};
+}
